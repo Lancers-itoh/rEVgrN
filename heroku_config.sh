3c3be5fb70466295e8cf84e6f5d01826303d8cc6
itotoma <riboosu@icloud.com>
2020-11-28 09:42:08 +0900

    test

---
+++ heroku_config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+heroku config:set SECRET_KEY="knt5e_sj_95sr3g7oa#cdvq3eqjgp)u^^4j*a(e)6&a5$b4j=u" --app keiba-ml
+heroku config:set APP_EMAIL_ADRESS="kensuke.k1150@gmail.com" --app keiba-ml
+heroku config:set APP_EMAIL_PASSWORD="lcxufugfmtnmdssm" --app keiba-ml
